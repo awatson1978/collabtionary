bffd6613c2bd820f7b4940f070e87676047c5f4c
Abigail Watson <awatson1978@gmail.com>
2014-02-06 16:17:09 -0500

    Update subscriptions.js

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,10 +1,2 @@
-// the subscription needs to be in an autosubscribe to detect the reactive Session variable
-
-//Meteor.autosubscribe(function(){
-//    Meteor.subscribe('customerAccounts', Session.get('account_search_term'));
-//    Meteor.subscribe('dowjones', Session.get('day_summary_search'));
-//
-//});
-
 Meteor.subscribe('usersDirectory');
 Meteor.subscribe('userProfile', Meteor.userId());
